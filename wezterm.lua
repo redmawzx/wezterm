ca3b6fff721f086097397906537cfdc4e2ddcb94
Jan-Andreas Rusnak <42783523+redmawzx@users.noreply.github.com>
2024-05-21 23:54:57 +0200

    color scheme, fonts and appearance

--- wezterm.lua
+++ wezterm.lua
@@ -3,4 +3,19 @@ local wezterm = require("wezterm")
 
 return {
 	default_domain = "WSL:Ubuntu",
+	color_scheme = "Tokyo Night Moon",
+	font_size = 14.0,
+	line_height = 1.2,
+	font = wezterm.font_with_fallback({
+		"FantasqueSansM Nerd Font",
+		"JetBrainsMono Nerd Font",
+		"ComicCodeLigatures Nerd Font",
+	}),
+	-- Remove terminal padding
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
 }
